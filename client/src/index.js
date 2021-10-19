edb4bcfa365727a6ce53b41ab44ff4e521afc062
Askar Rizvi <askarrizvi88@gmail.com>
2021-10-19 16:44:30 -0400

    Implement the client files and fix some of the server files, add ApolloProvider to fix invariant violation error

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+// Need to create an ApolloClient to start the client
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  // Need to use ApolloProvider to fix Invariant Violation error
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+    </ApolloProvider>,
   document.getElementById('root')
 );
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,21 +3,7 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-type Query {
-    me: User
-    users: [User]
-    user(username: String!): User
-    savedBooks(username: String): [Book]
-  }
-
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: [String]!, email: String!, password: String!): Auth
-    saveBook(authors:String!, description: String!, title: String!, bookId: String!, image: String!, link: String!): User
-    removeBook(bookId: String!): User
-  }
-
-  type User {
+type User {
     _id: ID
     username: String
     email: String
@@ -37,6 +23,27 @@ type Query {
   type Auth {
     token: ID!
     user: User
+  }
+
+
+input BookDetails {
+  bookId: String
+  authors: [String]
+  description: String
+  title: String
+  image: String
+  link: String
+}
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: BookDetails!): User
+    removeBook(bookId: String!): User
   }
 `;
 
